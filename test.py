52aa5c9b4756487003d4cb3e8a4e226d0ff41792
Sfatimah01 <sfatimahhl001@gmail.com>
2021-09-13 16:31:21 +0700

    test

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, url_for, request
 from werkzeug.utils import redirect
 
@@ -8,13 +9,35 @@ def index():
     if request.method=='POST':
         email = request.form['email']
         password = request.form['password']
-        if email == "rizki@gmail.com" and password == "r" :
-            return render_template('sukses.html')
+        if email == "siti@gmail.com" and password == "r" :
+            return redirect('/sukses')
         else:
             return redirect("/gagal")
     return render_template('index.html')
+
 @application.route('/gagal')
 def gagal():
     return render_template("home.html")
+
+@application.route('/sukses')
+def sukses():
+    return render_template("sukses.html")
+
+@application.route('/home')
+def Beranda():
+    return render_template("home.html")
+
+@application.route('/proses',methods = ['GET','POST'])
+def upload():
+    if request.method=='POST':
+        file = request.file['file']
+        file.save(os.path.join("static/uploads", file.filename))
+        return render_template('upload.html', message="Berhasil")
+    return render_template('sukses.html')
+
+
+@application.route('/signup')
+def signup():
+    return render_template("Signup.html")
 if __name__ == '__main__':
     application.run(debug=True)
